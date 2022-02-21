77be0c1aeb15d2804375399513cc5f94870b9b39
이현세 <dlgustp1487@naver.com>
2022-02-21 17:32:27 +0900

    생성자 주입을 선택해라!

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -14,21 +14,13 @@ public class OrderServiceImpl implements OrderService{
 	private final MemberRepository memberRepository;
 	private final DiscountPolicy discountPolicy;
 	
+	
 	@Autowired
 	public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
 		this.memberRepository = memberRepository;
 		this.discountPolicy = discountPolicy;
 	}
 	
-	
-//	@Autowired
-//	public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-//		this.memberRepository = memberRepository;
-//		this.discountPolicy = discountPolicy;
-//	}
-
-
-
 	@Override
 	public Order createOrder(Long memberId, String itemName, int itemPrice) {
 		Member member = memberRepository.findById(memberId);
