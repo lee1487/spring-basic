473b68ca203e00bbee1a637aea3aa52e4fb232c9
이현세 <dlgustp1487@naver.com>
2022-02-21 10:06:57 +0900

    싱글톤 컨테이너

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -53,11 +53,11 @@ class SingletonTest {
 		// 2.조회: 호출할 때 마다 객체를 생성
 		MemberService memberService2 = ac.getBean("memberService", MemberService.class);
 
-		//참조값이 다른 것을 확인
+		//참조값이 같은 것을 확인
 		System.out.println("memberService1 = " + memberService1);
 		System.out.println("memberService2 = " + memberService2);
 
-		// memberService1 != memberService2
+		// memberService1 == memberService2
 		assertThat(memberService1).isSameAs(memberService2);
 	}
 
