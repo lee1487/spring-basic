d8083944b4784835d81b8fbc93d7d859768172d7
이현세 <dlgustp1487@naver.com>
2022-02-21 13:02:05 +0900

    컴포넌트 스캔과 의존관계 자동 주입 시작하기

---
+++ src/test/java/hello/core/scan/AutoAppConfigTest.java
@@ -0,0 +1,22 @@
+package hello.core.scan;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import hello.core.AutoAppConfig;
+import hello.core.member.MemberService;
+
+class AutoAppConfigTest {
+
+	@Test
+	void basicScan() {
+		AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
+		
+		MemberService memberService = ac.getBean(MemberService.class);
+		assertThat(memberService).isInstanceOf(MemberService.class);
+	}
+
+}
