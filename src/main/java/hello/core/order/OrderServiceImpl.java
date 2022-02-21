07118ba47ca2e25d1b20c9e6d83eb211818e9ecc
이현세 <dlgustp1487@naver.com>
2022-02-21 18:05:25 +0900

    조회 빈이 2개 이상 - 문제

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package hello.core.order;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import hello.core.discount.DiscountPolicy;
@@ -8,17 +9,16 @@ import hello.core.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
 
 @Component
-@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
 
 	private final MemberRepository memberRepository;
 	private final DiscountPolicy discountPolicy;
 	
-	
-//	public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-//		this.memberRepository = memberRepository;
-//		this.discountPolicy = discountPolicy;
-//	}
+	@Autowired
+	public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+		this.memberRepository = memberRepository;
+		this.discountPolicy = discountPolicy;
+	}
 	
 	@Override
 	public Order createOrder(Long memberId, String itemName, int itemPrice) {
